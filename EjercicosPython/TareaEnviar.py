6691c014f7a694953cd15390fc95fb67da2d2fac
SofiaGarzon <161245912+SofiaGarzon@users.noreply.github.com>
2024-08-14 22:32:47 +0000

    tarea15/08

---
+++ EjercicosPython/TareaEnviar.py
@@ -0,0 +1,156 @@
+#Realizado por: Karen Sofia Garzon Rubiano. Cod: 6001090
+
+""" Desarrolla un programa que calcule el total de ventas de un día, el promedio de ventas y la venta
+más alta. El usuario debe ingresar el número de ventas del día y luego el valor de cada venta.
+Solicitar al usuario el número total de ventas del día. Calcular y mostrar el total de ventas, el
+promedio y la venta más alta. Mínimo 10 entradas."""
+
+
+#Funcion que calcula el total de las ventas por dia
+def sumatoria_ventas(ventas):
+    sumatoria = 0
+
+    #Recorrer todas las ventas y calcula su sumatoria
+    for i in range(len(ventas)):
+        sumatoria += ventas[i]
+
+    return sumatoria
+
+#Calcula el promedio de las ventas
+def promedio_ventas(sumatoria, total_ventas):
+    return sumatoria / total_ventas
+
+#Funcion que captura las ventas dependiendo del total de ventas ingresadas
+def capturar_ventas(total_ventas):
+    #Array que va a guardar los precios ingresados
+    ventas = []
+    
+    #Se captura cada una de las ventas
+    for i in range(total_ventas):
+        valor_venta = int(input(f"Digite el valor para la venta {i + 1}: "))
+        #Se guarda el valor de la venta en el array
+        ventas.append(valor_venta)
+        
+    sumatoria_total = sumatoria_ventas(ventas)
+    print(f"El total de ventas es: {sumatoria_total}")
+    
+    promedio = promedio_ventas(sumatoria_total, total_ventas)
+    print(f"El promedio de las ventas es: {promedio}")
+    
+    #La funcion sort organiza un array de menor a mayor, quedando la venta mayor en la ultima posicion del array
+    ventas.sort()
+    
+    #Se obtiene la venta mayor que esta en la ultima posicion del array
+    venta_mayor = ventas[len(ventas) - 1]
+    print(f"La venta mas alta fue: {venta_mayor}")
+    
+    
+       
+#Funcion principal del programa
+def main():
+    #Ciclo infinito que solo se rompe cuando el valor ingresado es valido
+    while True: 
+        total_ventas = int(input("Digite el numero de ventas en el día: "))
+        
+        if total_ventas >= 10:
+            #Se llama la funcion que calcula el total de ventas ingresadas
+            capturar_ventas(total_ventas)
+            break
+        else:
+            print("El numero de ventas debe ser igual o mayor que 10")
+    
+
+main()
+
+
+
+
+
+
+
+
+
+"""Escribe un programa que genere y muestre la tabla de multiplicar hasta 10 para cualquier número
+que el usuario ingrese. Pedir al usuario que ingrese un número. Utilizar un bucle para generar las
+multiplicaciones y mostrar los resultados."""
+
+#Se hace el metodo principal
+def main():
+    numero_tabla = int(input("Digite el numero del cual quiere saber la tabla: "))
+    
+    #Se iteran los numeros de 0 a 10, se pone +1 para empezar desde 1
+    for i in range(10):
+        print(f"{numero_tabla} x {i + 1} = {numero_tabla * (i + 1)}")
+    
+main()
+
+
+
+
+
+
+
+
+"""Implementa un sistema que lea las calificaciones de los estudiantes de un curso y determine la
+calificación promedio, la calificación más alta y la más baja. El usuario debe ingresar el número de
+estudiantes y luego la calificación de cada uno. Solicitar al usuario el número de estudiantes.
+Utilizar bucles y estructuras condicionales para procesar y comparar las calificaciones. Calcular y
+mostrar la calificación promedio, la máxima y la mínima. Mínimo 6 ingresos."""
+
+#Funcion que calcula la sumatoria de las notas de los estudiantes 
+def sumatoria_calificaciones(calificaciones):
+    sumatoria = 0
+
+    #Recoje todas las calificaciones y calcula su sumatoria
+    for i in range(len(calificaciones)):
+        sumatoria += calificaciones[i]
+
+    return sumatoria
+
+#Calcula el promedio de las notas
+def promedio_calificaciones(sumatoria, estudiantes):
+    return sumatoria / estudiantes
+
+#Funcion que captura las calificaciones dependiendo del total de estudiantes ingresados
+def num_estudiantes(estudiantes):
+    #Array que va a guardar las calificaciones ingresadas
+    calificaciones = []
+    
+    #Se captura cada una de las calificaciones de los estudiantes
+    for i in range(estudiantes):
+        nota_individual = float(input(f"Digite la calificacion del estudiante {i + 1}: "))
+        #Se guarda el valor de la nota en el array
+        calificaciones.append(nota_individual)
+        
+    sumatoria_total = sumatoria_calificaciones(calificaciones)
+    
+    promedio = promedio_calificaciones(sumatoria_total, estudiantes)
+    print(f"El promedio de las notas de los estudiantes del curso es: {promedio}")
+    
+    #La funcion sort organiza un array de menor a mayor, quedando la calificacion mayor en la ultima posicion del array
+    calificaciones.sort()
+    
+    #Se obtiene la calificacion mayor que esta en la ultima posicion del array
+    calificacion_mayor = calificaciones[len(calificaciones) - 1]
+    print(f"La calificacion mas alta fue: {calificacion_mayor}")
+    
+    calificacion_menor = calificaciones[0]
+    print(f"La calificacion mas baja fue: {calificacion_menor}")
+    
+    
+       
+#Funcion principal del programa
+def main():
+    #Ciclo infinito que solo se rompe cuando el valor ingresado es valido
+    while True: 
+        estudiantes = int(input("Digite el numero de estudiantes: "))
+        
+        if estudiantes >= 6:
+            #Se llama la funcion que calcula el total de calificaciones ingresadas
+            num_estudiantes(estudiantes)
+            break
+        else:
+            print("Debe recibir la nota de minimo 6 estudiantes")
+    
+
+main()
